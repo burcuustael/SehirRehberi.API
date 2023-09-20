ff4606b0278c781232ffd9a1f33912f9ab60578b
burcuustael <burcustael@gmail.com>
2023-09-20 22:25:00 +0300

    Şehir Detaylarının Getirilmesi ve Reference Looping Hatası

---
+++ SehirRehberi.API/Data/AppRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SehirRehberi.API.Models;
+
+namespace SehirRehberi.API.Data
+{
+    public class AppRepository : IAppRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public AppRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public void Add<T>(T entity) where T : class 
+        {
+            _dataContext.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _dataContext.Remove(entity);
+        }
+
+        public List<City> GetCities()
+        {
+            var cities = _dataContext.Cities.Include(c=>c.Photos).ToList();
+            return cities;
+        }
+
+        public City GetCityById(int cityId)
+        {
+            var city = _dataContext.Cities.Include(c => c.Photos).FirstOrDefault(c=>c.Id == cityId);
+            return city;
+        }
+
+        public Photo GetPhoto(int id)
+        {
+            var photo = _dataContext.Photos.FirstOrDefault(c=>c.Id == id);
+            return photo;
+        }
+
+        public List<Photo> GetPhotosByCity(int cityId)
+        {
+            var photos = _dataContext.Photos.Include(p=>p.CityId==cityId).ToList();
+            return photos;
+        }
+
+        public bool SaveAll()
+        {
+            return _dataContext.SaveChanges() > 0;
+        }
+    }
+}
--- SehirRehberi.API/Program.cs
+++ SehirRehberi.API/Program.cs
@@ -1,11 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SehirRehberi.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["DefaultConnection"]);
+});
 
-builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(Program));
+
+
+builder.Services.AddScoped<IAppRepository, AppRepository>();
+
+//þehrin fotolarýnýn detaylarý ile çekilirken oluþan loop u önlemek için eklenir...
+
+builder.Services.AddControllers().AddJsonOptions(opt =>
+{
+    opt.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+});
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
